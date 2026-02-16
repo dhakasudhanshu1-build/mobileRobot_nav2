d1ccaa503a1fd59b797709b12ed57caa68391ae8
dhakasudhanshu1-build <dhakasudhanshu1@gmail.com>
2026-02-16 23:44:01 +0530

    creted a pkg for navigation to user specifird position

---
+++ navigation_client_ui/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'navigation_client_ui'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='sudhanshu',
+    maintainer_email='dhakasudhanshu1@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'nav_to_pose = navigation_client_ui.navigate_to_pose_node:main',
+        ],
+    },
+)
